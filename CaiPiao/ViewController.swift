7224929f8744fe45de93588e310ab90b81988747
luyinuo <501886861@qq.com>
2018-01-29 18:30:11 +0800

    彩票
    彩票系统

--- CaiPiao/ViewController.swift
+++ CaiPiao/ViewController.swift
@@ -7,19 +7,22 @@
 //
 
 import UIKit
-
+#if Environment_Mode
+let SERVER_URL = "http://dev.server.com/api/"
+let API_TOKEN = "DI2023409jf90ew"
+#else
+let SERVER_URL = "http://prod.server.com/api/"
+let API_TOKEN = "71a629j0f090232"
+#endif
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSLog("\(SERVER_URL)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
