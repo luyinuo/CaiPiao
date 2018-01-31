83b8b4b47e2c99df1d1912a6a7e46040cb7a0649
luyinuo <501886861@qq.com>
2018-01-31 18:05:27 +0800

    首页完善

---
+++ CaiPiao/CPCommonUtil.swift
@@ -0,0 +1,12 @@
+//
+//  CPCommonUtil.swift
+//  CaiPiao
+//
+//  Created by kevinlu on 2018/1/31.
+//  Copyright © 2018年 otvcloud. All rights reserved.
+//
+
+import UIKit
+
+let kScreenW = UIScreen.main.bounds.width
+let kScreenH = UIScreen.main.bounds.height
--- CaiPiao/CaiPiaoOne/ViewController.swift
+++ CaiPiao/CaiPiaoOne/ViewController.swift
@@ -16,10 +16,26 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         NSLog("\(SERVER_URL)")
         self.tableView.delegate = self
         self.tableView.dataSource = self
+        var headerViewHeight:CGFloat = 0.0
+        if(UIDevice.current.userInterfaceIdiom == .pad) {
+            headerViewHeight = kScreenW / 5.0 * 2 + 10
+        }else{
+            headerViewHeight = kScreenW / 3.0 * 3 + 10
+        }
+        self.tableView.tableHeaderView?.height = headerViewHeight;
+        self.fetchNews()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentify", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
     }
     
 }
@@ -41,6 +57,27 @@ extension UIViewController {
             case let .failure(error):
                 print("请求失败"+error.localizedDescription)
             }
+        }
+    }
+    func fetchNews (){
+        
+        let path = Bundle.main.path(forResource:"news", ofType: "json")
+        
+        let url = URL(fileURLWithPath: path!)
+        
+        do{
+            
+            let data = try Data(contentsOf: url)
+            
+            let json:Any = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions.mutableContainers)
+            
+            let datalist = json as! NSArray
+            print(datalist)//输出数据
+            
+        }catch let erro as Error!{
+            
+            print("读取本地数据出现错误！",erro)
+            
         }
     }
 }
